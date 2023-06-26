5cab6b9f638d6331f0cab075fe37f1903e1a48db
kbao <kbao122003@gmail.com>
2023-06-26 11:49:04 +0700

    use handlebars ,reconstruct folder and add module type into project

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { engine } from 'express-handlebars';
+
+const app = express();
+const port = 3000
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//http logger
+app.use(morgan('combined'))
+
+//template engine
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, './resources/views'))
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.render('home')
+})
+app.get('/news', (req, res) => {
+  res.render('news')
+})
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
