50c270df9124e0017841d01c1eced83c9942e44d
kbao <kbao122003@gmail.com>
2023-07-04 02:26:24 +0700

    remove css public folder

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const port = 3000
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(express.static(path.join(__dirname, '/public')))
+
 //http logger
 app.use(morgan('combined'))
 
